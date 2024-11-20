4b775544ad166568a00103f8a449d53359d9ebcf
Jonathan Bruce <me@jonathanbruce.ca>
2024-11-19 23:18:12 -0500

    Add build scripts

---
+++ Build Scripts/install.js
@@ -0,0 +1,88 @@
+var series = require("async/series");
+var path = require("path");
+
+var tasks = require("./tasks.js");
+
+var npmInstall = project => {
+  return done => {
+    var exec = require("child_process").exec;
+
+    var child = exec(
+      "npm install",
+      {
+        cwd: project.directory
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          done(error);
+          return;
+        }
+
+        console.log(`npm install done for ${project.name}`);
+
+        if (stdout) {
+          console.log(stdout);
+        }
+
+        if (stderr) {
+          console.error(stderr);
+        }
+
+        var nodeModulesPath = path.join(project.directory, "node_modules");
+        var powerShellModules = require("glob").sync(
+          path.join(project.directory, "node_modules", "**", "*.psm1")
+        );
+
+        if (powerShellModules.length > 0) {
+          var fs = require("fs-extra");
+          var taskFilePath = path.join(project.directory, "task.json");
+          var task = fs.existsSync(taskFilePath)
+            ? fs.readJsonSync(taskFilePath)
+            : {};
+
+          if (task.execution.PowerShell3) {
+            var psModulesPath = path.join(project.directory, "ps_modules");
+            fs.ensureDirSync(psModulesPath);
+
+            for (var i = 0; i < powerShellModules.length; i++) {
+              var powerShellModulePath = powerShellModules[i];
+              var powerShellModuleDirName = path.dirname(powerShellModulePath);
+              var powerShellModuleFolderName = path.basename(
+                powerShellModuleDirName
+              );
+              fs.copySync(
+                powerShellModuleDirName,
+                path.join(psModulesPath, powerShellModuleFolderName),
+                { clobber: true, dereference: true }
+              );
+              console.log(
+                `${powerShellModuleFolderName} copied in ps_modules for ${
+                  project.name
+                }`
+              );
+            }
+          }
+        }
+
+        done();
+      }
+    );
+  };
+};
+
+var installTasks = tasks.getTasks().map(npmInstall);
+
+installTasks.unshift(
+  npmInstall({
+    directory: __dirname,
+    name: "BuildScripts"
+  })
+);
+
+series(installTasks, err => {
+  if (err) {
+    console.error("Failed to install child dependencies");
+    throw err;
+  }
+});
---
+++ Build Scripts/prebuild.js
@@ -0,0 +1,56 @@
+var fs = require("fs-extra");
+var path = require("path");
+var tasks = require("./tasks.js");
+var collection = require("lodash/collection");
+
+var currentDirectory = process.cwd();
+var nodeCommonFilesRoot = path.join(currentDirectory, "Common", "Node");
+var powershellCommonFilesRoot = path.join(
+  currentDirectory,
+  "Common",
+  "PowerShell3"
+);
+var tasksRoot = path.join(currentDirectory, "Tasks");
+
+var nodeFiles = fs.existsSync(nodeCommonFilesRoot)
+  ? fs.readdirSync(nodeCommonFilesRoot)
+  : [];
+var powershellFiles = fs.existsSync(powershellCommonFilesRoot)
+  ? fs.readdirSync(powershellCommonFilesRoot)
+  : [];
+
+collection.forEach(tasks.getTasks(), task => {
+  var targetNodeCommonDir = path.join(task.directory, "common");
+  var taskNodeModules = path.join(task.directory, "node_modules");
+  var targetPowershellCommonDir = path.join(task.directory, "ps_modules");
+
+  var taskFilePath = path.join(task.directory, "task.json");
+  var taskFile = fs.existsSync(taskFilePath)
+    ? fs.readJsonSync(taskFilePath)
+    : {};
+
+  if (taskFile.execution.Node) {
+    fs.ensureDirSync(targetNodeCommonDir);
+    fs.ensureDirSync(taskNodeModules);
+    collection.forEach(nodeFiles, commonFile => {
+      var targetFile = path.join(targetNodeCommonDir, commonFile);
+      console.log(targetFile);
+      fs.copySync(path.join(nodeCommonFilesRoot, commonFile), targetFile, {
+        clobber: true
+      });
+    });
+  }
+
+  if (taskFile.execution.PowerShell3) {
+    fs.ensureDirSync(targetPowershellCommonDir);
+    collection.forEach(powershellFiles, commonFile => {
+      var targetFile = path.join(targetPowershellCommonDir, commonFile);
+      console.log(targetFile);
+      fs.copySync(
+        path.join(powershellCommonFilesRoot, commonFile),
+        targetFile,
+        { clobber: true }
+      );
+    });
+  }
+});
